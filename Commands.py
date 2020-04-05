cba1f4c480bb2430b154e3c89df95bacfd9b57e4
mluperon <41967033+mluperon@users.noreply.github.com>
2020-04-05 12:57:45 -0400

    Create Commands.py

---
+++ Commands.py
@@ -0,0 +1,16 @@
+## Code Framework for ECE 369 Group Project
+
+
+## Framework for View -
+## Command to pull all information currently on server (document )
+
+def View(portnumber, document):
+
+
+
+
+
+## Framework for Create -
+## Command to upload notes as they are being sent to server 
+
+def Create(medium , color):
